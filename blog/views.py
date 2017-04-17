5a79d954182afd5e730e927129b8ab4bf018b646
TylwythTeg <garciarobertoiv@yahoo.com>
2017-04-16 23:18:22 -0700

    Got blog posts working at /blog/ points to /blog/index.html which is a list of blog posts

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseRedirect
+from django.utils import timezone
+from .models import Post
 
 # Create your views here.
 
@@ -7,6 +9,13 @@ from django.http import HttpResponse, HttpResponseRedirect
 
 #post_list
 def index(request):
+
+	posts = Post.objects.order_by('-published_date')
+
+	return render(request, 'blog/post_list.html', {'posts': posts})
+
+
+	
 
 	return render(request, 'blog/post_list.html', {})
 	#return HttpResponse("Blog hey")
